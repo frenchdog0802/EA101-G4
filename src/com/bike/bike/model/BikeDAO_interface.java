c12535d7ffff8bdcfcb37a1d3a53f8836437c565
Bert <x6815541@gmail.com>
2020-06-23 17:37:34 +0800

    到rentMaster 選擇數量

--- src/com/bike/bike/model/BikeDAO_interface.java
+++ src/com/bike/bike/model/BikeDAO_interface.java
@@ -10,4 +10,5 @@ public interface BikeDAO_interface {
 	public List<BikeVO> getAll();
 	public Integer findStoreBikeEmpty(String sq_bike_store_id); //找出店家目前空的車輛
 	public List<String> findStoreBikeType(String sq_bike_store_id);//找出店家目前的車種
+	
 }
