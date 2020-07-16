21cb191b089f50ff2298a9f2f03fbd49fe44e111
annemom <e877654@gmail.com>
2020-07-16 12:21:27 +0800

    討論區後台 ALL UPDATE 芷薇

--- src/com/forum/controller/ForumServlet.java
+++ src/com/forum/controller/ForumServlet.java
@@ -134,6 +134,38 @@ public class ForumServlet extends HttpServlet {
 				failureView.forward(req, res);
 			}
 		}
+		
+		
+		if ("getOne_For_Update_Staff".equals(action)) { // 來自listAllForum.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/*************************** 1.接收請求參數 ****************************************/
+				String sq_theme_id = new String(req.getParameter("sq_theme_id"));
+				System.out.println("getOne_For_Update getsq_theme_id =" + sq_theme_id);
+				/*************************** 2.開始查詢資料 ****************************************/
+				ForumService forumSvc = new ForumService();
+				ForumVO forumVO = forumSvc.getOneTheme(sq_theme_id);
+				System.out.println("forumVO.getSq_theme_id=" + forumVO.getSq_theme_id());
+				System.out.println("forumVO.getSq_member_id=" + forumVO.getSq_member_id());
+
+				/*************************** 3.查詢完成,準備轉交(Send the Success view) ************/
+				req.setAttribute("updateListOne", forumVO); // 資料庫取出的memVO物件,存入req
+				String url2 = "/back-end/forum/updateListOne.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url2);// 成功轉交 update_emp_input.jsp
+				successView.forward(req, res);
+
+				/*************************** 其他可能的錯誤處理 **********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/forum/listAllForum.jsp");
+				failureView.forward(req, res);
+			}
+		}
 
 		if ("update".equals(action)) { // 來自update_Theme_input.jsp的請求
 
@@ -224,6 +256,101 @@ public class ForumServlet extends HttpServlet {
 				failureView.forward(req, res);
 			}
 		}
+		
+		
+		if ("updateByStaff".equals(action)) { // 來自updateListOne.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+				String sq_theme_id = new String(req.getParameter("sq_theme_id").trim());
+				System.out.println("update getsq_theme_id=" + sq_theme_id);
+
+				String sq_member_id = new String(req.getParameter("sq_member_id").trim());
+				System.out.println("update getsq_member_id=" + sq_member_id);
+
+				String theme_name = new String(req.getParameter("theme_name").trim());
+//				String theme_nameReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{2,16}$";
+				if (theme_name == null || theme_name.trim().length() == 0) {
+					errorMsgs.add("主題名稱: 請勿空白");
+				}
+//				else if (!theme_name.trim().matches(theme_nameReg)) { // 以下練習正則(規)表示式(regular-expression)
+//					errorMsgs.add("主題名稱: 只能是中、英文字母、數字和_ , 且長度必需在2到16之間");
+//				}
+
+				String theme_detial = req.getParameter("theme_detial").trim();
+				if (theme_detial == null || theme_detial.trim().length() == 0) {
+					errorMsgs.add("主題內容請勿空白");
+				}
+				
+				Integer theme_display_status = Integer.parseInt(req.getParameter("theme_display_status").trim());
+				;
+				System.out.println(req.getParameter("theme_display_status"));
+				System.out.println(theme_display_status);
+				
+
+				InputStream in = req.getPart("theme_pic").getInputStream();
+				byte[] theme_pic = null;
+				if (session.getAttribute("theme_pic") == null) {
+					if (in.available() == 0) {
+						ForumService ForumSrc = new ForumService();
+						ForumVO forumVO = ForumSrc.getOneTheme(sq_theme_id);
+						theme_pic = forumVO.getTheme_pic();
+					} else {
+						theme_pic = new byte[in.available()];
+						session.setAttribute("theme_pic", theme_pic);
+						in.read(theme_pic);
+						in.close();
+					}
+				} else {
+					theme_pic = (byte[]) session.getAttribute("theme_pic");
+					in.read(theme_pic);
+					in.close();
+				}
+
+
+				ForumVO forumVO = new ForumVO();
+				forumVO.setSq_theme_id(sq_theme_id);
+				forumVO.setSq_member_id(sq_member_id);
+				forumVO.setTheme_name(theme_name);
+				forumVO.setTheme_detial(theme_detial);
+				forumVO.setTheme_pic(theme_pic);
+				forumVO.setTheme_display_status(theme_display_status);
+
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("update_Theme_input", forumVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					RequestDispatcher failureView = req.getRequestDispatcher("/back-end/forum/updateListOne.jsp");
+					failureView.forward(req, res);
+					return; // 程式中斷
+				}
+
+				/*************************** 2.開始修改資料 *****************************************/
+				ForumService forumSvc = new ForumService();
+				forumVO = forumSvc.updateTheme(sq_theme_id, sq_member_id, theme_name, theme_detial, theme_pic,
+						theme_display_status);
+				session.removeAttribute("theme_pic");
+
+				/*************************** 3.修改完成,準備轉交(Send the Success view) *************/
+				req.setAttribute("updateListOne", forumVO); // 資料庫update成功後,正確的的empVO物件,存入req
+				String url2 = "/back-end/forum/listAllForum.jsp";
+
+				RequestDispatcher successView = req.getRequestDispatcher(url2); // 修改成功後,轉交listOneEmp.jsp
+				successView.forward(req, res);
+
+				/*************************** 其他可能的錯誤處理 *************************************/
+
+			} catch (Exception e) {
+				errorMsgs.add("修改資料失敗:" + e.getMessage());
+				RequestDispatcher failureView = req.getRequestDispatcher("/back-end/forum/listAllForum.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
 
 		if ("insert".equals(action)) { // 來自addEmp.jsp的請求
 
